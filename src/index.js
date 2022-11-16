c9a37243ca839f72711ad55fc910934b87374ae0
duncan <dmckee95@gmail.com>
2022-11-16 12:08:52 -0500

    Create heading and nav items

--- src/index.js
+++ src/index.js
@@ -2,6 +2,32 @@ import loadHome from "./home";
 import loadMenu from "./menu"
 import loadContact from "./contact";
 
-// loadHome();
-// loadMenu();
+function heading (){
+    const header = document.createElement("div");
+    header.classList.add("header")
+    const logo = document.createElement("h1");
+    logo.textContent = "Grand Wine";
+    const menuItems = ["Home", "Our Menu", "Contact Us"];
+    const navMenu = document.createElement("ul");
+    navMenu.classList.add("navMenu")
+    menuItems.forEach(element => {
+        const item = document.createElement("li");
+        item.textContent = element;
+        navMenu.appendChild(item);
+    })
+
+    header.appendChild(logo);
+    header.appendChild(navMenu)
+
+    return header
+}
+function loadMe (item){
+    const content = document.getElementById("content");
+    content.appendChild(item);
+}
+
+loadMe(heading());
+
+loadHome();
+loadMenu();
 loadContact();
